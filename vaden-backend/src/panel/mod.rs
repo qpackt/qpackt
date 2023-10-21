0003d1dcf34593b325e447b2bb57b52ce65aee3d
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2023-10-21 19:13:47 +0200

    Refactor + license headers

--- vaden-backend/src/panel/mod.rs
+++ vaden-backend/src/panel/mod.rs
@@ -1,12 +1,32 @@
+// SPDX-License-Identifier: AGPL-3.0
+/*
+   Vaden: Versioned Application Deployment Engine
+   Copyright (C) 2023 Łukasz Wojtów
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use crate::config::Config;
 use crate::dao::Dao;
-use crate::panel::upload_version::upload_version;
+use crate::panel::versions::upload::upload_version;
 use crate::proxy::upstream::Upstreams;
 use actix_files::Files;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use tokio::task::JoinHandle;
-pub(super) mod upload_version;
+
+pub(super) mod versions;
 
 pub(super) fn start_panel_http(
     upstreams: Data<Upstreams>,
